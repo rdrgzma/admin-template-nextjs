5a775e459d7e0b75c394456d57be8899ee6986e1
rdrgzma <rdrgzma@hotmail.com>
2021-11-17 13:53:29 -0300

    fix development

--- src/components/template/Cabecalho.tsx
+++ src/components/template/Cabecalho.tsx
@@ -1,15 +1,14 @@
-import Titulo from './Titulo';
+import Titulo from "./Titulo";
 
 interface CabecalhoProps {
-    titulo: string;
-    subtitulo: string;
+  titulo: string;
+  subtitulo: string;
 }
 
 export default function Cabecalho(props: CabecalhoProps) {
-    return (
-        <div>
-           <Titulo titulo={props.titulo} subtitulo={props.subtitulo}/>
-
-        </div>
-    )
+  return (
+    <div>
+      <Titulo titulo={props.titulo} subtitulo={props.subtitulo} />
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import Layout from "../components/template/Layout"
+import Layout from "../components/template/Layout";
 export default function Home() {
-    return (
-        <Layout titulo='Página inicial' subtitulo='Contruindo o admin'>
-            <h1>Conteúdo!</h1>
-        </Layout>
-    )  
- }
+  return (
+    <Layout titulo="Página inicial" subtitulo="Contruindo o admin">
+      <h1>Conteúdo!</h1>
+    </Layout>
+  );
+}
