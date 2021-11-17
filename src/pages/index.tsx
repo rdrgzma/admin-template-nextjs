d077cbd667bd9c8d58447e49f20125db2dbe7bc3
rdrgzma <rdrgzma@hotmail.com>
2021-11-17 16:05:00 +0000

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import Layout from "../components/template/Layout"
+export default function Home() {
+    return (
+        <Layout titulo='Página inicial' subtitulo='Contruindo o admin'>
+            <h1>Conteúdo!</h1>
+        </Layout>
+    )  
+ }
