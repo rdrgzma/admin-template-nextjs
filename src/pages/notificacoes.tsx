f620fbe5cceb9baf96bb6c925079841212278dd5
rdrgzma <rdrgzma@hotmail.com>
2021-11-18 21:30:56 -0300

    fix pages

--- src/pages/notificacoes.tsx
+++
@@ -1,8 +0,0 @@
-import Layout from "../components/template/Layout";
-export default function Notificacoes() {
-  return (
-    <Layout titulo="Notificações " subtitulo="Notificações">
-      <h1>Notificações Aqui!</h1>
-    </Layout>
-  );
-}
---
+++ src/pages/notificacoes/index.tsx
@@ -0,0 +1,8 @@
+import Layout from "../../components/template/Layout";
+export default function Notificacoes() {
+  return (
+    <Layout titulo="Notificações" subtitulo="Notificações">
+      <h1>Notificações Aqui!</h1>
+    </Layout>
+  );
+}
